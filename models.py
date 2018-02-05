30615387b66c5dc0394e0d75cc503393f9bd47e9
Rhett D'souza <rhettdsouza13@gmail.com>
2018-02-05 17:15:16 -0500

    Decision Trees Added

--- models.py
+++ models.py
@@ -2,10 +2,12 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 from parser_enc import *
 from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 
 inputs, labels = input_inject_GLIOMA_MLP()
 set_br = 45
-set_br_SVM = 50
+set_br_SVM = 45
+set_br_DT = 50
 
 estimator = MLPClassifier(hidden_layer_sizes = (100), batch_size = 5, max_iter=1000, solver='adam')
 estimator.fit(inputs[:set_br], labels[:set_br])
@@ -44,3 +46,19 @@ for pred,lab in zip(predicted_sv, labels_SVM[set_br_SVM:]):
         continue
 accuracy_sv = float(float(correct)/len(labels_SVM[set_br_SVM:]))
 print accuracy_sv
+
+
+classifier = DecisionTreeClassifier(random_state=0)
+classifier.fit(inputs[:set_br_DT],labels_SVM[:set_br_DT])
+predicted_dt = classifier.predict(inputs[set_br_DT:])
+print predicted_dt
+
+correct = 0
+for pred,lab in zip(predicted_dt, labels_SVM[set_br_DT:]):
+    if pred == lab:
+        correct += 1
+    else:
+        continue
+
+accuracy_dt = float(float(correct)/len(labels_SVM[set_br_DT:]))
+print accuracy_dt
