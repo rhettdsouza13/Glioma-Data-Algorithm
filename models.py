5a2bbaa01953b34c6c7b7a5175e242a8d07612a4
Rhett D'souza <rhettdsouza13@gmail.com>
2018-02-08 17:43:39 -0500

    Counters Included

--- models.py
+++ models.py
@@ -7,9 +7,9 @@ from sklearn.tree import DecisionTreeClassifier
 inputs, labels = input_inject_GLIOMA_MLP()
 set_br = 45
 set_br_SVM = 45
-set_br_DT = 50
+set_br_DT = 45
 
-estimator = MLPClassifier(hidden_layer_sizes = (100), batch_size = 5, max_iter=1000, solver='adam')
+estimator = MLPClassifier(hidden_layer_sizes = (5), batch_size = 5, max_iter=1000, solver='adam')
 estimator.fit(inputs[:set_br], labels[:set_br])
 print "Trained"
 #
@@ -17,14 +17,31 @@ print "Trained"
 # print labels[60:]
 predicted = estimator.predict(inputs[set_br:])
 print predicted
+correct_1 = 0
+correct_0 = 0
+wrong_1 = 0
+wrong_0 = 0
 correct = 0
 for pred,lab in zip(predicted,labels[set_br:]):
     if pred[0] == lab[0] and pred[1] == lab[1]:
         correct += 1
+    if pred[0] == lab[0] and pred[1] == lab[1] and lab[0] == 0 :
+        correct_0 += 1
+    elif pred[0] == lab[0] and pred[1] == lab[1] and lab[0] == 1 :
+        correct_1 += 1
+    elif pred[0] != lab[0] and pred[1] != lab[1] and lab[0] == 1 :
+        wrong_1 += 1
+    elif pred[0] != lab[0] and pred[1] != lab[1] and lab[0] == 0 :
+        wrong_0 += 1
     else:
         continue
 accuracy = float(float(correct)/len(labels[set_br:]))
 print accuracy
+print correct_1 + correct_0 + wrong_0 + wrong_1
+print correct_1
+print correct_0
+print wrong_1
+print wrong_0
 # print estimator.score(inputs[60:], labels[60:])
 # print labels[set_br:]
 # print accuracy_score(labels[set_br:], predicted.argmax(axis=1))
@@ -38,14 +55,31 @@ clf.fit(inputs[:set_br_SVM], labels_SVM[:set_br_SVM])
 predicted_sv = clf.predict(inputs[set_br_SVM:])
 print predicted_sv
 
+correct_1 = 0
+correct_0 = 0
+wrong_1 = 0
+wrong_0 = 0
 correct = 0
 for pred,lab in zip(predicted_sv, labels_SVM[set_br_SVM:]):
     if pred == lab:
         correct += 1
+    if pred == lab and lab == 0 :
+        correct_0 += 1
+    elif pred == lab and lab == 1 :
+        correct_1 += 1
+    elif pred != lab and lab == 1 :
+        wrong_1 += 1
+    elif pred != lab and lab == 0 :
+        wrong_0 += 1
     else:
         continue
 accuracy_sv = float(float(correct)/len(labels_SVM[set_br_SVM:]))
 print accuracy_sv
+print correct_1 + correct_0 + wrong_0 + wrong_1
+print correct_1
+print correct_0
+print wrong_1
+print wrong_0
 
 
 classifier = DecisionTreeClassifier(random_state=0)
@@ -53,12 +87,29 @@ classifier.fit(inputs[:set_br_DT],labels_SVM[:set_br_DT])
 predicted_dt = classifier.predict(inputs[set_br_DT:])
 print predicted_dt
 
+correct_1 = 0
+correct_0 = 0
+wrong_1 = 0
+wrong_0 = 0
 correct = 0
 for pred,lab in zip(predicted_dt, labels_SVM[set_br_DT:]):
     if pred == lab:
         correct += 1
+    if pred == lab and lab == 0 :
+        correct_0 += 1
+    elif pred == lab and lab == 1 :
+        correct_1 += 1
+    elif pred != lab and lab == 1 :
+        wrong_1 += 1
+    elif pred != lab and lab == 0 :
+        wrong_0 += 1
     else:
         continue
 
 accuracy_dt = float(float(correct)/len(labels_SVM[set_br_DT:]))
 print accuracy_dt
+print correct_1 + correct_0 + wrong_0 + wrong_1
+print correct_1
+print correct_0
+print wrong_1
+print wrong_0
--- parser_enc.py
+++ parser_enc.py
@@ -2,21 +2,22 @@ import csv
 import numpy
 from datetime import datetime
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import normalize
 
 raw_data_set = []
-with open("shuffled_GLIOMA.csv", 'r') as g_d_csv:
+with open("Glioma Data.xlsx - Sheet1.csv", 'r') as g_d_csv:
     f_reader = csv.reader(g_d_csv, delimiter=' ', quotechar='|')
     for row in f_reader:
         raw_data_set.append(row[0].split(','))
 
-#
-# data_set = []
-# for sample in raw_data_set:
-#     d1 = sample.pop(0)
-#     d2 = sample[0]
-#     d1 = datetime.strptime(d1, "%m/%d/%Y")
-#     d2 = datetime.strptime(d2, "%m/%d/%Y")
-#     sample[0] = (abs((d2 - d1).days)/365)
+
+data_set = []
+for sample in raw_data_set:
+    d1 = sample.pop(0)
+    d2 = sample[0]
+    d1 = datetime.strptime(d1, "%m/%d/%Y")
+    d2 = datetime.strptime(d2, "%m/%d/%Y")
+    sample[0] = (abs((d2 - d1).days)/365)
 
 
 
@@ -53,17 +54,17 @@ for i,sample in enumerate(raw_data_set):
             raw_data_set[i][j] = 4
             continue
         if feature == 'RIP':
-            raw_data_set[i][j] = 0
-            continue
-        if feature == 'Alive':
-            raw_data_set[i][j] = 1
-            continue
-        if feature == '0' and j == 22:
             raw_data_set[i][j] = [0,1]
             continue
-        if feature == '1' and j == 22:
+        if feature == 'Alive':
             raw_data_set[i][j] = [1,0]
             continue
+        # if feature == '0' and j == 22:
+        #     raw_data_set[i][j] = [0,1]
+        #     continue
+        # if feature == '1' and j == 22:
+        #     raw_data_set[i][j] = [1,0]
+        #     continue
         raw_data_set[i][j] = float(raw_data_set[i][j])
 
 
@@ -71,11 +72,11 @@ for i,sample in enumerate(raw_data_set):
 labels = []
 inputs = []
 
-# numpy.random.shuffle(raw_data_set)
-# with open('shuffled_GLIOMA.csv', 'wb+') as file_store:
-#     wr = csv.writer(file_store)
-#     for row in raw_data_set:
-#         wr.writerow(row)
+numpy.random.shuffle(raw_data_set)
+with open('shuffled_GLIOMA.csv', 'wb+') as file_store:
+    wr = csv.writer(file_store)
+    for row in raw_data_set:
+        wr.writerow(row)
 
 print raw_data_set
 
@@ -83,10 +84,11 @@ for sample in raw_data_set:
     labels.append(sample[len(sample)-1])
     inputs.append(sample[:-1])
 
-scaler = MinMaxScaler()
+scaler = MinMaxScaler(feature_range = (0,1))
 scaler.fit(inputs)
 print scaler.data_max_
 inputs = scaler.transform(inputs)
+inputs = normalize(inputs)
 
 def input_inject_GLIOMA_MLP():
     return inputs, labels
